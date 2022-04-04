c80dfac3d4e7c6654a5472910cdb484d905ae368
shtabdeepaull <shtabdee30@gmail.com>
2022-04-04 21:48:53 +0600

    added favicon & home section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import img from '../image/img.png';
 
 const Home = () => {
     return (
-        <div>
-            <h2 className='text-4xl'>This is Home</h2>
+        <div className='flex m-6'>
+            <div className='mt-5'>
+            <h2 className='text-4xl font-semibold '>Laptop With Personality</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem et placeat doloribus, est recusandae quibusdam blanditiis rem doloremque laborum tempora.
+            </p>
+            <button className=' p-2 rounded mt-5 font-mono font-semibold bg-red-700 hover:bg-slate-500'>Live Demo</button>
+            </div>
+            <div>
+                <img src={img} alt="" />
+            </div>
         </div>
     );
 };
