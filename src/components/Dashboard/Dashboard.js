d2257b89fd77503254f61ec33ca2dfef51e4f9a5
shtabdeepaull <shtabdee30@gmail.com>
2022-04-05 01:14:30 +0600

    added chart in dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, Line, LineChart, BarChart, Bar, RadialBarChart, RadialBar ,Legend, Tooltip, XAxis, YAxis } from 'recharts';
+
 
 const Dashboard = () => {
+    const [chart, setChart] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setChart(data))
+    }, []);
+
     return (
-        <div>
-            <h2>This is Dashboard</h2>
+        <div className='grid grid-cols-2 mt-20'>
+            <div className='m-5'>
+                <p className='font-bold text-left ml-20 mb-4'>LineChart</p>
+                <LineChart width={300} height={300} data={chart}>
+                    <Line dataKey={"investment"}></Line>
+                    <Line dataKey={"revenue"}></Line>
+                    <XAxis dataKey={"month"}></XAxis>
+                    <Tooltip></Tooltip>
+                    <YAxis></YAxis>
+                </LineChart>
+            </div>
+
+            <div className='m-5'>
+                <p className='font-bold text-left ml-20 mb-4'>AreaChart</p>
+                <AreaChart width={300} height={300} data={chart}>
+                    <Area dataKey={"investment"}></Area>
+                    <Area dataKey={"revenue"}></Area>
+                    <XAxis dataKey={"month"}></XAxis>
+                    <Tooltip></Tooltip>
+                    <Legend></Legend>
+                    <YAxis></YAxis>
+                </AreaChart>
+            </div>
+            <div className='m-5'>
+                <p className='font-bold text-left ml-20 mb-4'>BarChart</p>
+                <BarChart width={300} height={300} data={chart}>
+                    <XAxis dataKey="month"></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                    <Legend></Legend>
+                    <Bar dataKey="revenue" fill='#82ca9d'></Bar>
+                    <Bar dataKey={"sell"} fill="#8884d8"></Bar>
+                </BarChart>
+            </div>
+            <div className='m-5'>
+                <p className='font-bold text-left ml-20 mb-4'>RadialBar Chart</p>
+                <RadialBarChart width={300} height={300} data={chart} innerRadius="10%" outerRadius="80%" startAngle={180} endAngle={0}>
+                    <RadialBar minAngle={15} label={{ fill: '#8884d8', position: 'insideStart' }} background clockWise={true} dataKey='investment' ></RadialBar>
+                    <Legend />
+                    <Tooltip></Tooltip>
+                </RadialBarChart>
+            </div>
         </div>
+
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,13 @@ import img from '../image/img.png';
 
 const Home = () => {
     return (
-        <div className='flex m-6'>
-            <div className='mt-5'>
-            <h2 className='text-4xl font-semibold '>Laptop With Personality</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem et placeat doloribus, est recusandae quibusdam blanditiis rem doloremque laborum tempora.
-            </p>
-            <button className=' p-2 rounded mt-5 font-mono font-semibold bg-red-700 hover:bg-slate-500'>Live Demo</button>
+        <div className='grid gap-x-5 grid-cols-2  m-6'>
+            <div className='mt-20'>
+                <h2 className='text-4xl font-semibold '>Laptop With Personality</h2>
+                <p className='font-mono'>Laptop has a place in our heart when it's about our work and gaming.
+                    And we bring you the most powerful machine that's worth buying at a fare price. Here, we will discuss about the best laptops of 2022. We will help you to decide which one you should buy from your recommendations.
+                </p>
+                <button className=' p-2 rounded mt-5 font-mono font-semibold bg-red-700 hover:bg-slate-500'>Live Demo</button>
             </div>
             <div>
                 <img src={img} alt="" />
