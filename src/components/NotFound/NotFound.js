44b2fc8e47f41700160dd33109ee7237ab66d695
shtabdeepaull <shtabdee30@gmail.com>
2022-04-05 15:27:58 +0600

    edit 404 route

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className='text-2xl bg-red-800 font-bold'>
             <h2>The Page You Are Looking Is Not Found!!</h2>
         </div>
     );
